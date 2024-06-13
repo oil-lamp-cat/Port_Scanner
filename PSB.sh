ad19a8eb320f5ed1d00c724d8b9f15d2a0afb233
oil-lamp-cat <103806022+oil-lamp-cat@users.noreply.github.com>
2024-06-14 00:17:39 +0900

    getopts

--- PSB.sh
+++ PSB.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
-IP_ADDRESS=${1:-"127.0.0.1"}
+IP_ADDRESS="127.0.0.1"
 
-PORT_START=${2:-1}
+PORT_START=1
 
-PORT_END=${3:-1024}
+PORT_END=1024
+
+SERVICE=false
+
+OPTIONS=":i:ah?"
+
+usage()
+{
+    echo '
+    <options>
+    -i [ip] : ip설정
+    -a : 서비스에 관한 설명까지 추가되어 출력
+    -h : 도움말
+    '
+}
+
+while getopts $OPTIONS opts; do
+    case $opts in
+    \?)
+    echo "invalid option"
+    usage
+    exit 1;;
+    a) SERVICE=true
+    ;;
+    i) IP_ADDRESS=$OPTARG
+    ;;
+    h)
+        usage
+    exit;;
+    :)
+        usage
+    exit;;
+    esac
+done
 
 scan_port(){
     PORT=$1
-    (echo >/dev/tcp/"$IP_ADDRESS"/"$PORT") &>/dev/null && echo -e "$PORT/tcp\topen\t`grep $PORT/tcp /etc/services | head -n1 | awk '{print $1}'`"
+    if [ $SERVICE = true ]; then
+        (echo >/dev/tcp/"$IP_ADDRESS"/"$PORT") &>/dev/null && echo -e "$PORT/tcp\topen\t`grep $PORT/tcp /etc/services | head -n1 | awk '{print $1}'`\t`grep $PORT/tcp /etc/services | head -n1 | awk '{print $4}'`"
+    else
+        (echo >/dev/tcp/"$IP_ADDRESS"/"$PORT") &>/dev/null && echo -e "$PORT/tcp\topen\t`grep $PORT/tcp /etc/services | head -n1 | awk '{print $1}'`"
+    fi
     sleep 0.01
 }
 
